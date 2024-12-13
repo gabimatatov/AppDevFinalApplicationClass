1f66e246173b8c1145f7d4583304cdaecd595901
gabimatatov <gavriel.matatov@veeva.com>
2024-12-13 14:10:59 +0200

    Initial Commit

---
+++ app/src/main/java/com/example/finalapplicationclass/MainActivity.kt
@@ -0,0 +1,25 @@
+package com.example.finalapplicationclass
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_main)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+
+        // TODO: Step 1 - Add Student Button
+        // TODO: Step 2 - Nvigaet to AddStudentAcrivity
+        // TODO: Step 3 - Save Student
+        // TOD
+    }
+}
